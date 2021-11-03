a314cb8c71162a01b9b0f691ab321f8eddac32ac
Liliia <liliia.pohorielova@nure.ua>
2021-11-04 00:28:51 +0200

    Create Bean and get its Name

---
+++ app-1/src/main/java/ua/com/pogorelova/TestSpring.java
@@ -0,0 +1,17 @@
+package ua.com.pogorelova;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        //штука которая считает файлик applicationContext.xml и положит созданный Бин в аппликейшн контекст
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        //достать бин из аппликейшн контекста
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
+    }
+}
